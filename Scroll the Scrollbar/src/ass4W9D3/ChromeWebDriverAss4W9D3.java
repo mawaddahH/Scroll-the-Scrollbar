a638cee8b03d627dcced49f376b8209e9793c0a6
lo0ol <lo0ol@MWDH-Work2>
2022-08-03 01:03:07 +0300

    First Commit

---
+++ Scroll the Scrollbar/src/ass4W9D3/ChromeWebDriverAss4W9D3.java
@@ -0,0 +1,59 @@
+package ass4W9D3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class ChromeWebDriverAss4W9D3 {
+	public WebDriver driver;
+
+	/**
+	 * Set up browser settings and open the application
+	 * 
+	 * @throws InterruptedException
+	 */
+
+	@BeforeTest
+	public void setUp() throws InterruptedException {
+		// the path for open WebSite
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\lo0ol\\" + "Downloads\\Compressed\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://zeplin.io/");
+		System.out.println(driver.getTitle());
+	}
+
+	/**
+	 * Test Scrollbar
+	 * 
+	 * @throws InterruptedException
+	 */
+	@Test
+	public void LearnEnglishWebSite() throws InterruptedException {
+
+		// Scrollbar to the specific element
+		WebElement checkbox1 = driver.findElement(By.xpath("//a[normalize-space()='Get started for free']"));
+		checkbox1.sendKeys(Keys.END);
+		Thread.sleep(2000);
+
+	}
+
+	/**
+	 * Tear down the setup after test completes
+	 */
+	@AfterTest
+	public void terminateBrowser() {
+		// Close the browser
+		driver.close();
+
+		// Quite the selenium
+		driver.quit();
+	}
+}
